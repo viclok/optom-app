9a14babfb4945d1e2bd807b921212feef60f804a
viclok <110517011+viclok@users.noreply.github.com>
2025-03-16 13:04:05 +1100

    Added all buttons

---
+++ README.md
@@ -0,0 +1,9 @@
+Tasks to be completed
+1. ~~Create tabs for the various modes~~ 
+2. Create the buttons
+3. Layout and colour the buttons
+4. Create the stopwatch
+
+
+Priorities
+- fix N-Ret button  
--- controls.js
+++ controls.js
@@ -1,14 +1,90 @@
-// const buttons = document.getElementsByClassName("optomNoteButton")
 const copyBox = document.getElementById("copyBox")
+const buttonDict = new Map([
+    ["Near Phoria", "Phoria @ 33cm"],
+    ["Far Phoria", "Phoria @ 3m"],
+    ["+2", "Under +2:"],
+    ["-2", "Under -2:"],
+    ["BI/BO", ".8BI/12BO facility"]
+])
+var copyState = []
+var backup = []
+var useBackup = false
+var addNewline = false
 
+// Functions
 function add_to_note(button) {
-    copyBox.value += (button.innerText + " ")
-    console.log(copyBox.value)
+    var buttonClasses = button.classList
+    if (buttonClasses.contains("undoButton")) {
+        if (useBackup) {
+            copyState = backup
+            useBackup = false
+        } else if (addNewline) {
+            addNewline = false
+        } else {
+            copyState.pop()
+        }
+    } 
+    else if (buttonClasses.contains("radioButton")) {
+        copyState.push(button.value)
+    }
+    else if (buttonClasses.contains("clearButton")) {
+        backup = copyState
+        copyState = []
+        useBackup = true
+    }
+    else if (buttonClasses.contains("newlineButton")) {
+        backup = copyState
+        addNewline = true
+        return
+    }
+    else if (buttonClasses.contains("phoriaButton")) {
+        copyState.push(button.innerText + " " + (Number(button.innerText) % 2 == 0 ? "exo" : "eso"))
+    }
+    else if (buttonClasses.contains("facilityButton")) {
+        copyState.push(button.innerText + " facility")
+    }
+    else if (buttonClasses.contains("optomNoteButton")) {
+        copyState.push((buttonDict.has(button.innerText) ? buttonDict.get(button.innerText): button.innerText))
+    }
+    if (addNewline) {
+        copyState[copyState.length - 1] = "\n" + copyState[copyState.length - 1]
+        addNewline = false
+    }
+    copyBox.value = copyState.join(" ")
 }
 
+function openTab(button) {
+    // Declare all variables
+    var i, tabcontent, tablinks;
+
+    // Get all elements with class="tabcontent" and hide them
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+    }
+
+    // Get all elements with class="tablinks" and remove the class "active"
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+
+    // Show the current tab, and add an "active" class to the button that opened the tab
+    document.getElementById(button.innerText).style.display = "block";
+    button.className += " active";
+}
+
+
+// Event Listeners
 document.querySelectorAll(".optomNoteButton").forEach((button) => {
     button.addEventListener("click", function() {
         add_to_note(button)
+    })
+})
+
+document.querySelectorAll(".tablinks").forEach((button) => {
+    button.addEventListener("click", function() {
+        openTab(button)
     })
 })
 
@@ -16,10 +92,13 @@ document.querySelector(".copyButton").addEventListener("click", () => {
     navigator.clipboard.writeText(copyBox.value)
 })
 
-// buttons[0].addEventListener('click', add_to_note(buttons[0]))
-// console.log(buttons.type)
-// buttons.forEach((button) => {
-//     button.addEventListener("click", function() {
-//         add_to_note(button)
-//     })
-// });
+document.querySelector(".nRetButton").addEventListener("click", () => {
+    document.querySelector(".nretOptions").style.display = "block"
+})
+
+document.querySelector(".closeButton").addEventListener("click", () => {
+    document.querySelector(".nretOptions").style.display = "none"
+})
+// Other
+document.getElementById("defaultTab").click()
+console.log("h")
