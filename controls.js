672ebe66253ff348231d2eeaa3af2509cbf69363
viclok <110517011+viclok@users.noreply.github.com>
2025-04-06 00:46:14 +1100

    finished first page

--- controls.js
+++ controls.js
@@ -6,10 +6,13 @@ const buttonDict = new Map([
     ["-2", "Under -2:"],
     ["BI/BO", "8BI/12BO facility"]
 ])
+const rainAPIURL = "https://api.open-meteo.com/v1/forecast?latitude=-37.9713&longitude=146.9837&daily=rain_sum&timezone=auto&forecast_days=1"
+
 var copyState = []
 var backup = []
 var useBackup = false
 var addNewline = false
+var useCM = false
 
 // Functions
 function add_to_note(button) {
@@ -23,7 +26,27 @@ function add_to_note(button) {
         } else {
             copyState.pop()
         }
+    }
+    else if  (buttonClasses.contains("checkboxButton")) {
+        useCM = !useCM
     } 
+    else if (buttonClasses.contains("rainButton")) {
+        fetch(rainAPIURL)
+            .then(response => {
+                if (!response.ok) {
+                // throw new Error('Network response was not ok');
+                    console.log("Error with API")
+                }
+                return response.json();
+            })
+            .then(data => {
+                console.log(data.daily.rain_sum[0]);
+                copyState.push(`Rainfall today is ${data.daily.rain_sum[0]}mm in Maffra`)
+            })
+            .catch(error => {
+                console.error('Error:', error);
+            });
+    }
     else if (buttonClasses.contains("radioButton")) {
         copyState.push(button.value)
     }
@@ -38,7 +61,11 @@ function add_to_note(button) {
         return
     }
     else if (buttonClasses.contains("phoriaButton")) {
-        copyState.push(button.innerText + " " + (Number(button.innerText) % 2 == 0 ? "exo" : "eso"))
+        var suffix = "cm"
+        if (!useCM) {
+            suffix = (Number(button.innerText) % 2 == 0 ? " exo" : " eso")
+        }
+        copyState.push(button.innerText + suffix)
     }
     else if (buttonClasses.contains("facilityButton")) {
         copyState.push(button.innerText + " facility")
@@ -48,6 +75,7 @@ function add_to_note(button) {
     }
     if (addNewline) {
         copyState[copyState.length - 1] = "\n" + copyState[copyState.length - 1]
+        console.log(copyState)
         addNewline = false
     }
     copyBox.value = copyState.join(" ")
@@ -74,6 +102,23 @@ function openTab(button) {
     button.className += " active";
 }
 
+function updateTime() {
+    var date = new Date()
+    var day = date.getDate();
+    var month = date.getMonth() + 1;
+    var year = date.getFullYear();
+    var s = date.getSeconds();
+    var m = date.getMinutes();
+    var h = date.getHours();
+
+    // This arrangement can be altered based on how we want the date's format to appear.
+    var currentDate = `${day}-${month}-${year} ` + ("0" + h).slice(-2) + ":" + ("0" + m).slice(-2) + ":" + ("0" + s).slice(-2);
+    // console.log(currentDate); // "17-6-2022"
+    document.querySelector(".datetime").innerHTML = currentDate
+}
+
+setInterval(updateTime, 1000)
+
 
 // Event Listeners
 document.querySelectorAll(".optomNoteButton").forEach((button) => {
@@ -95,6 +140,7 @@ document.querySelector(".copyButton").addEventListener("click", () => {
 document.querySelector(".nRetButton").addEventListener("click", () => {
     document.querySelector(".nretOptions").style.display = "block"
 })
+
 
 // Other
 document.getElementById("defaultTab").click()
